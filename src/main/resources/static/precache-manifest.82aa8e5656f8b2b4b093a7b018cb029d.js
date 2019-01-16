76a4f5b67842c4176d4fc77358d1c1f710608d13
Faxiang Li <lifa0917@gmail.com>
2019-01-16 09:33:28 -0500

    Add react frontend

---
+++ src/main/resources/static/precache-manifest.82aa8e5656f8b2b4b093a7b018cb029d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e7a35aa7240ff4e63dd",
+    "url": "/static/js/main.3e7a35aa.chunk.js"
+  },
+  {
+    "revision": "538ae2c7cbef1e0d0e8c",
+    "url": "/static/js/1.538ae2c7.chunk.js"
+  },
+  {
+    "revision": "3e7a35aa7240ff4e63dd",
+    "url": "/static/css/main.f3e7f09b.chunk.css"
+  },
+  {
+    "revision": "538ae2c7cbef1e0d0e8c",
+    "url": "/static/css/1.2dc5333d.chunk.css"
+  },
+  {
+    "revision": "6315fbc9b27aee0a749180302d59ce7c",
+    "url": "/index.html"
+  }
+];
